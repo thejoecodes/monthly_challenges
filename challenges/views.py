67138a43c3787a22f5c692f419556f17fd98f172
Okall Joeshem <okalljosh@gmail.com>
2021-12-08 18:50:43 +0300

    add try except to return monthly challenges

--- challenges/views.py
+++ challenges/views.py
@@ -23,19 +23,9 @@ def monthly_challenge_by_numbers(request, month):
     return HttpResponse(month)
 
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == 'january':
-        challenge_text ='Earn AWS Certification'
-    elif month == 'february':
-        challenge_text = 'Finish on the Django Course/Creating the Monthly Challenges App'
-    elif month == 'march':
-        challenge_text = 'Learn React and Make the Monthly Challenges App Front End'
-    elif month == 'april':
-        challenge_text = 'Learn Docker'
-    elif month == 'may':
-        challenge_text = 'Start Blogging'
-    elif month == 'june':
-        challenge_text = 'Grow my Twitter Audience @kokallj'
-    else:
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound("This Month is Not Supported")
-    return HttpResponse(challenge_text)
+    
