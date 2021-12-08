bf5cf15ea7c13f38402402fcde9617663c5f550c
Okall Joeshem <okalljosh@gmail.com>
2021-12-08 15:05:15 +0300

    Add monthly challenges dictionary

--- challenges/views.py
+++ challenges/views.py
@@ -2,6 +2,21 @@ from django.http.response import HttpResponseNotFound
 from django.shortcuts import render
 from django.http import HttpResponse
 
+monthly_challenges ={
+    "january":"Earn AWS Certification",
+    "february": "Finish on the Django Course/Creating the Monthly Challenges App",
+    "march":"Review Javascript Fundermentals, Learn React and Make the Monthly Challenges App Front End",
+    "april":"Learn Docker",
+    "may":"Start Blogging",
+    "june":"Grow my Twitter Audience @kokallj",
+    "july":"Start Looking for a Dev Job (Fullstack, Django Backend heavy)",
+    "august":"Start on Data Engineering",
+    "september":"Create a libary of Data Engineering Resources",
+    "october":"Create/Build a Blog Site",
+    "november":"Compile thoughts and resources on Data Engineering/Data Science/Django and React",
+    "december": "Write a Comprehensive Review of the Journey from the Monthly Summaries"
+}
+
 # Create your views here.
 
 def monthly_challenge_by_numbers(request, month):
