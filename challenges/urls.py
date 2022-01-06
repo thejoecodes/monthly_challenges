a5db06b3cb0f2015008404d34fb01bf3d0ab5512
Okall Joeshem <okalljosh@gmail.com>
2022-01-06 12:00:42 +0300

    Fix index/home link error

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="index"),
     path('<int:month>', views.monthly_challenge_by_numbers),
     path('<str:month>', views.monthly_challenge, name="monthly-challenge")
 ]
